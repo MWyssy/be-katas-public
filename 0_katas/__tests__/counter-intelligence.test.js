b59f256f6ea5afde5772a00929cba4716db7afee
MWyssy <mike@wyss.co.uk>
2023-04-26 17:18:59 +0300

    add counter-intellifence tests and solution

--- 0_katas/__tests__/counter-intelligence.test.js
+++ 0_katas/__tests__/counter-intelligence.test.js
@@ -1,7 +1,19 @@
 const { counterIntelligence } = require('../counter-intelligence');
 
 describe('counterIntelligence()', () => {
-  it('', () => {
-    // first test here
+  it('should return a string', () => {
+    expect(counterIntelligence()).toBe('')
+  })
+  it('should return: "\'X\' - the string has been shifted by 1". When passsed a single letter Y', () => {
+    expect(counterIntelligence('Y')).toBe('"X" - the string has been shifted by 1.')
+  });
+  it('should account for a multi letter string, shifted by 1', () => {
+    expect(counterIntelligence('BCD Y')).toBe('"ABC X" - the string has been shifted by 1.')
+  });
+  it('should account for strings shifted by more than 1', () => {
+    expect(counterIntelligence('MNO d')).toBe('"ABC X" - the string has been shifted by 12.')
+  });
+  it('should work if the letters have been shifted down', () => {
+    expect(counterIntelligence('ANVNVKNA CX YRLT DY IDLLQRWR XW HXDA FJH QXVN OAXV FXAT, MJAURWP G')).toBe('"REMEMBER TO PICK UP ZUCCHINI ON YOUR WAY HOME FROM WORK, DARLING X" - the string has been shifted by -17.')
   });
 });
--- 0_katas/counter-intelligence.js
+++ 0_katas/counter-intelligence.js
@@ -1,6 +1,28 @@
 // Please do not change the name of this function
 const counterIntelligence = (str) => {
-  // Your code here
+  if (!str || str.length === 0) return '';
+
+  const strArr = str.split('');
+
+  const lastLetter = str[str.length - 1];
+  let numberShifted = lastLetter.charCodeAt(0) - 'X'.charCodeAt(0);
+  
+  let decodedMessage = strArr.map((letter) => {
+    if (/[A-Z]/i.test(letter)) {
+      let decodedLetter = String.fromCharCode((letter.charCodeAt(0) - numberShifted))
+      if (decodedLetter.charCodeAt(0) > 90 && numberShifted < 0) {
+        let leftToShift = decodedLetter.charCodeAt(0) - 91;
+        decodedLetter = String.fromCharCode(65 + leftToShift)
+      } else if (decodedLetter.charCodeAt(0) < 65 && numberShifted > 0) {
+        let leftToShift = decodedLetter.charCodeAt(0) - 65
+        decodedLetter = String.fromCharCode(90 + leftToShift)
+      }
+      return decodedLetter
+    }
+    return letter
+  }).join('');
+
+  return `"${decodedMessage}" - the string has been shifted by ${numberShifted}.`
 };
 
 module.exports = { counterIntelligence };
