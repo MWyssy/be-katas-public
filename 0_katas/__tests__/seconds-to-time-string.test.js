6888c801015a0863800a5500a35db736094dcdb4
MWyssy <mike@wyss.co.uk>
2023-04-26 23:27:53 +0300

    add seconds-to-time-string tests and solution

--- 0_katas/__tests__/seconds-to-time-string.test.js
+++ 0_katas/__tests__/seconds-to-time-string.test.js
@@ -1,7 +1,31 @@
 const { secondsToTimeString } = require('../seconds-to-time-string');
 
 describe('secondsToTimeString()', () => {
-  it('', () => {
-    // first test here
+  it('Returns a string', () => {
+    expect(secondsToTimeString()).toBe('')
+  });
+  it('Returns "1 second" when passed the number 1', () => {
+    expect(secondsToTimeString(1)).toBe('1 second')
+  });
+  it('Returns the plural "seconds", when passed a number larger than 1', () => {
+    expect(secondsToTimeString(5)).toBe('5 seconds')
+  });
+  it('Can account for minutes', () => {
+    expect(secondsToTimeString(60)).toBe('1 minute')
+  });
+  it('Can account for hours', () => {
+    expect(secondsToTimeString(3610)).toBe('1 hour, and 10 seconds')
+  });
+  it('Can account for days', () => {
+    expect(secondsToTimeString(361000)).toBe('4 days, 4 hours, 16 minutes, and 40 seconds')
+  });
+  it('Can account for weeks', () => {
+    expect(secondsToTimeString(761000)).toBe('1 week, 1 day, 19 hours, 23 minutes, and 20 seconds')
+  });
+  it('Can account for months', () => {
+    expect(secondsToTimeString(7610010)).toBe('2 months, 3 weeks, 6 days, 5 hours, 53 minutes, and 30 seconds')
+  });
+  it('Can account for years', () => {
+    expect(secondsToTimeString(76100101)).toBe('2 years, 4 months, 4 weeks, 1 day, 2 hours, 55 minutes, and 1 second')
   });
 });
--- 0_katas/seconds-to-time-string.js
+++ 0_katas/seconds-to-time-string.js
@@ -1,6 +1,65 @@
 // Please do not change the name of this function
-const secondsToTimeString = (seconds) => {
-  // Your code here
+const secondsToTimeString = (secs) => {
+  let result = ''
+  if (!secs) return result
+  
+  const time = {
+    year: 0,
+    month: 0,
+    week: 0,
+    day: 0,
+    hour: 0,
+    minute: 0,
+    second: secs
+  }
+
+  if (time.second === 1) return `1 second`
+
+  if (time.second > 59) {
+    while (time.second > 59) {
+      if (time.second > 31536000) {
+        time.year++
+        time.second -= 31536000
+      } else if (time.second > 2628000) {
+        time.month++
+        time.second -= 2628000
+      } else if (time.second > 604800) {
+        time.week++
+        time.second -= 604800
+      } else if (time.second > 86400) {
+        time.day++
+        time.second -= 86400
+      } else if (time.second > 3600) {
+        time.hour++
+        time.second -= 3600
+      } else {
+        time.minute++
+        time.second -= 60;
+      };
+    }
+  }
+
+  for (let timeframe in time) {
+    if (timeframe === 'second' && result.length !== 0) {
+      if (time[timeframe] === 1) {
+        result += `, and ${time[timeframe]} ${timeframe}`
+      }
+      if (time[timeframe] > 1) {
+        result += `, and ${time[timeframe]} ${timeframe}s`
+      }
+    } else {
+      if (time[timeframe] === 1) {
+        if (result.length > 0) result += `, ${time[timeframe]} ${timeframe}`
+        else result += `${time[timeframe]} ${timeframe}`
+      }
+      if (time[timeframe] > 1) {
+        if (result.length > 0) result += `, ${time[timeframe]} ${timeframe}s`
+        else result += `${time[timeframe]} ${timeframe}s`
+      }
+    }
+  }
+
+  return result
 };
 
 module.exports = { secondsToTimeString };
