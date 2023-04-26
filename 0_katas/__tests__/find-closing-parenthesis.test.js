e31c12248e8ac3df3552684c9830a9769a435ea4
MWyssy <mike@wyss.co.uk>
2023-04-26 17:52:55 +0300

    add find-closing-parenthesis tests and solution

--- 0_katas/__tests__/find-closing-parenthesis.test.js
+++ 0_katas/__tests__/find-closing-parenthesis.test.js
@@ -1,7 +1,22 @@
 const { findClosingParenthesis } = require('../find-closing-parenthesis')
 
 describe('findClosingParenthesis()', () => {
-  it('', () => {
-    // first test here
+  it('Returns a number', () => {
+    expect(findClosingParenthesis('')).toBe(-1)
+  })
+  it('Returns a -1 when there are no parentheses present', () => {
+    expect(findClosingParenthesis('Hello!')).toBe(-1)
+  })
+  it('Returns the index of the closing parenthesis when there is no second argument, or the argument is 1', () => {
+    expect(findClosingParenthesis('(Hello!)', 1)).toBe(7)
+  })
+  it('Returns the index of the first closing parenthesis when there is no second argument, or the argument is 1, if there are mutliple parentheses', () => {
+    expect(findClosingParenthesis('Hello, (world). (Something Else).', 1)).toBe(13)
+  })
+  it('Returns the index of the closing bracket of the nth parentheses based on the second argument given', () => {
+    expect(findClosingParenthesis('Hello, (world). (Something Else).', 2)).toBe(31)
+  })
+  it('Works with more than two pairs of parentheses', () => {
+    expect(findClosingParenthesis('(Hello) (world) (!).', 3)).toBe(18)
   })
 })
--- 0_katas/find-closing-parenthesis.js
+++ 0_katas/find-closing-parenthesis.js
@@ -1,6 +1,20 @@
 // Please do not change the name of this function
-const findClosingParenthesis = (str) => {
-  // Your code here
+const findClosingParenthesis = (str, n) => {
+  let index = -1
+  
+  function getIndex(string, search, index) {
+    return string.split(search, index).join(search).length
+  }
+
+  if (/[()]/g.test(str)) {
+    if (n <= 1) {
+      index = str.indexOf(')');
+    } else {
+      index = getIndex(str, ')', n)
+    }
+  };
+
+  return index
 }
 
 module.exports = { findClosingParenthesis }
