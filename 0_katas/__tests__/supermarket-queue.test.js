44b825be65c978d0287bdc3df9c9c344afc17b92
MWyssy <mike@wyss.co.uk>
2023-05-03 19:09:43 +0300

    add supermarket-queue tests and partial solution

--- 0_katas/__tests__/supermarket-queue.test.js
+++ 0_katas/__tests__/supermarket-queue.test.js
@@ -1,7 +1,18 @@
-const { supermarketQueue } = require('../supermarket-queue')
+const { supermarketQueue } = require('../supermarket-queue');
 
 describe('supermarketQueue()', () => {
-  it('', () => {
-    // first test here
-  })
-})
+  it('returns a number', () => {
+    expect(supermarketQueue()).toBe(0);
+  });
+  it('returns the sum of all the numbers in the array, when the second argument is one.', () => {
+    expect(supermarketQueue([2, 2, 2], 1)).toBe(6);
+  });
+  it('returns the highest value in the array is the second argument is equal to the number of customers', () => {
+    expect(supermarketQueue([2, 10], 2)).toBe(10);
+  });
+  it('When there are fewer tills than customers, the customers will use a till in order of where they are in the array, and the result will be the length of time until all tills become free again.', () => {
+    expect(supermarketQueue([2, 2, 2], 2)).toBe(4);
+    expect(supermarketQueue([2, 3, 10], 2)).toBe(12);
+    expect(supermarketQueue([2, 3, 10, 4, 2, 5, 1], 2)).toBe(14);
+  });
+});
--- 0_katas/supermarket-queue.js
+++ 0_katas/supermarket-queue.js
@@ -1,6 +1,36 @@
 // Please do not change the name of this function
 const supermarketQueue = (customers, checkouts) => {
-  // Your code here
-}
+  let queueTime = 0;
+  if (!customers || customers.length === 0) return queueTime;
+  const copyCustomers = [...customers];
 
-module.exports = { supermarketQueue }
+  if (checkouts === 1)
+    return customers.reduce((sum, customer) => sum + customer, 0);
+
+  if (checkouts < copyCustomers.length) {
+    const checkoutsInUse = copyCustomers.splice(0, checkouts);
+    let lowestValue = 999999;
+    let longestQueuetime = 0;
+    checkoutsInUse.forEach((customer) => {
+      if (customer < lowestValue) lowestValue = customer;
+    });
+    longestQueuetime += lowestValue + copyCustomers[0];
+
+    if (copyCustomers.length <= 1) {
+      queueTime += longestQueuetime;
+    } else {
+      queueTime +=
+        supermarketQueue(copyCustomers.slice(1), checkouts) + longestQueuetime;
+    }
+  } else {
+    let highestValue = 0;
+    copyCustomers.forEach((customer) => {
+      if (customer > highestValue) highestValue = customer;
+    });
+    queueTime = highestValue;
+  }
+
+  return queueTime;
+};
+
+module.exports = { supermarketQueue };
