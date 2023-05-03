4ecf45a0e4b4184bf3f1e9ebdba708555b6fde94
MWyssy <mike@wyss.co.uk>
2023-05-03 16:15:23 +0300

    add treat-doggos tests and solutions

--- 1_data_manipulation/__tests__/treat-doggos.test.js
+++ 1_data_manipulation/__tests__/treat-doggos.test.js
@@ -1,7 +1,47 @@
 const treatDoggos = require('../treat-doggos.js');
 
 describe('treatDoggos', () => {
-  it('', () => {
-    // first test here
+  it('returns an array', () => {
+    expect(treatDoggos()).toEqual([]);
+  });
+  it('returns a new array', () => {
+    const dogs = [
+      { name: 'Otis', age: 3, hasRabies: true },
+      { name: 'Bobby', age: 6, hasRabies: true },
+      { name: 'Roxy', age: 1, hasRabies: true }
+    ];
+    expect(treatDoggos(dogs)).not.toBe(dogs);
+  });
+  it('changes the hasRabies value of one dog to false', () => {
+    const dogs = [{ name: 'Otis', age: 3, hasRabies: true }];
+    expect(treatDoggos(dogs)).toEqual([
+      { name: 'Otis', age: 3, hasRabies: false }
+    ]);
+  });
+  it('will work with mutliple dogs', () => {
+    const dogs = [
+      { name: 'Otis', age: 3, hasRabies: true },
+      { name: 'Bobby', age: 6, hasRabies: true },
+      { name: 'Roxy', age: 1, hasRabies: true }
+    ];
+    expect(treatDoggos(dogs)).toEqual([
+      { name: 'Otis', age: 3, hasRabies: false },
+      { name: 'Bobby', age: 6, hasRabies: false },
+      { name: 'Roxy', age: 1, hasRabies: false }
+    ]);
+  });
+  it('does not mutate the original array, or any of the original objects', () => {
+    const dogs = [
+      { name: 'Otis', age: 3, hasRabies: true },
+      { name: 'Bobby', age: 6, hasRabies: true },
+      { name: 'Roxy', age: 1, hasRabies: true }
+    ];
+    const copyDogs = [...dogs];
+    const copyFirstDog = { ...dogs[0] };
+
+    treatDoggos(dogs);
+
+    expect(dogs).toEqual(copyDogs);
+    expect(copyFirstDog).toEqual(copyDogs[0]);
   });
 });
--- 1_data_manipulation/treat-doggos.js
+++ 1_data_manipulation/treat-doggos.js
@@ -1,3 +1,12 @@
-const treatDoggos = (dogs) => {};
+const treatDoggos = (dogs) => {
+  if (!dogs || dogs.length === 0) return [];
+  const result = [...dogs];
+
+  return result.map((dog) => {
+    const copyDog = { ...dog };
+    copyDog.hasRabies = false;
+    return copyDog;
+  });
+};
 
 module.exports = treatDoggos;
