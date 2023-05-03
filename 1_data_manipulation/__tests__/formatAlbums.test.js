7efd36639c3a74ead9e9bc4a1cfca3e4fe910af9
MWyssy <mike@wyss.co.uk>
2023-05-03 16:05:54 +0300

    add format-albums tests and solution

--- 1_data_manipulation/__tests__/formatAlbums.test.js
+++ 1_data_manipulation/__tests__/formatAlbums.test.js
@@ -1,7 +1,49 @@
 const formatAlbums = require('../format-albums.js');
 
 describe('formatAlbums', () => {
-  it('', () => {
-    // first test here
+  it('returns an array', () => {
+    expect(formatAlbums()).toEqual([]);
+  });
+  it('returns a new array', () => {
+    const inputArr = [
+      { name: 'Grammatics', artist: 'Grammatics', releaseYear: 2009 },
+      { name: 'Kingdom of Rust', artist: 'Doves', releaseYear: 2009 }
+    ];
+    const artistIdReference = {
+      Grammatics: 9923,
+      Doves: 324
+    };
+    expect(formatAlbums(inputArr, artistIdReference)).not.toBe(inputArr);
+  });
+  it('changes the artist key to the artist id in the lookupObject', () => {
+    const inputArr = [
+      { name: 'Grammatics', artist: 'Grammatics', releaseYear: 2009 },
+      { name: 'Kingdom of Rust', artist: 'Doves', releaseYear: 2009 }
+    ];
+    const artistIdReference = {
+      Grammatics: 9923,
+      Doves: 324
+    };
+    expect(formatAlbums(inputArr, artistIdReference)).toEqual([
+      { name: 'Grammatics', artistID: 9923, releaseYear: 2009 },
+      { name: 'Kingdom of Rust', artistID: 324, releaseYear: 2009 }
+    ]);
+  });
+  it("doesn't mutate the original array, or the objects it contains", () => {
+    const inputArr = [
+      { name: 'Grammatics', artist: 'Grammatics', releaseYear: 2009 },
+      { name: 'Kingdom of Rust', artist: 'Doves', releaseYear: 2009 }
+    ];
+    const artistIdReference = {
+      Grammatics: 9923,
+      Doves: 324
+    };
+    const copyInputArr = [...inputArr];
+    const copyObjOne = { ...inputArr[0] };
+
+    formatAlbums(inputArr, artistIdReference);
+
+    expect(inputArr).toEqual(copyInputArr);
+    expect(copyObjOne).toEqual(copyInputArr[0]);
   });
 });
--- 1_data_manipulation/format-albums.js
+++ 1_data_manipulation/format-albums.js
@@ -1,3 +1,18 @@
-const formatAlbums = () => {};
+const formatAlbums = (arr, lookupObj) => {
+  if (!arr || arr.length === 0) return [];
+  const result = [...arr];
+
+  return result.map((item) => {
+    const copyItem = { ...item };
+    copyItem.artistID = lookupObj[copyItem.artist];
+    delete copyItem.artist;
+
+    return {
+      name: copyItem.name,
+      artistID: copyItem.artistID,
+      releaseYear: copyItem.releaseYear
+    };
+  });
+};
 
 module.exports = formatAlbums;
